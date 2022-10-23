c7df327594f9b9e4e6f17f3380fae831ac2bf04c
Tomas <tomas.kozakas@gmail.com>
2022-10-23 12:58:26 +0300

    Make every graph start point to be centered on y-axis

--- src/main/java/org/calculator/draw/Graph.java
+++ src/main/java/org/calculator/draw/Graph.java
@@ -9,7 +9,7 @@ import java.util.List;
  * @author Tomas Kozakas
  */
 public class Graph extends JPanel {
-    private static final int MAX_SCORE = 20000;
+    private int maxScore;
     private static final int PREF_W = 800;
     private static final int PREF_H = 650;
     private static final int BORDER_GAP = 30;
@@ -22,21 +22,22 @@ public class Graph extends JPanel {
 
     public Graph(List<Integer> scores) {
         this.scores = scores;
+        this.maxScore = scores.get(0) * 2;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D)g;
+        Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         double xScale = ((double) getWidth() - 2 * BORDER_GAP) / (scores.size() - 1);
-        double yScale = ((double) getHeight() - 2 * BORDER_GAP) / (MAX_SCORE - 1);
+        double yScale = ((double) getHeight() - 2 * BORDER_GAP) / (maxScore - 1);
 
         List<Point> graphPoints = new ArrayList<>();
         for (int i = 0; i < scores.size(); i++) {
             int x1 = (int) (i * xScale + BORDER_GAP);
-            int y1 = (int) ((MAX_SCORE - scores.get(i)) * yScale + BORDER_GAP);
+            int y1 = (int) ((maxScore - scores.get(i)) * yScale + BORDER_GAP);
             graphPoints.add(new Point(x1, y1));
         }
 
