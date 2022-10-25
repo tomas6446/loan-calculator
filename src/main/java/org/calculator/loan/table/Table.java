ec2e61650b194da48e4fb8ff40c2b76895ac85f4
Tomas <tomas.kozakas@gmail.com>
2022-10-25 18:47:56 +0300

    Make Filter.java and Table.java return floating point

---
+++ src/main/java/org/calculator/loan/table/Table.java
@@ -0,0 +1,40 @@
+package org.calculator.loan.table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Tomas Kozakas
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public final class Table {
+    private List<Row> rows = new ArrayList<>();
+
+    public void addRow(double initialBalance, double payment, double interestRate, double debtPart, double balanceLeft) {
+        rows.add(new Row(initialBalance, payment, interestRate, debtPart, balanceLeft));
+    }
+
+    public Row getRow(int index) {
+        return rows.get(index);
+    }
+
+    public List<Double> getColumn(String name) {
+        List<Double> rowsTemp = new ArrayList<>();
+        for (Row row : rows)
+            switch (name) {
+                case "initial" -> rowsTemp.add(row.initialBalance());
+                case "payment" -> rowsTemp.add(row.payment());
+                case "interest" -> rowsTemp.add(row.interestRate());
+                case "debt part" -> rowsTemp.add(row.debtPart());
+                case "balance left" -> rowsTemp.add(row.balanceLeft());
+                default -> throw new IllegalStateException("Unexpected value: " + name);
+            }
+        return rowsTemp;
+    }
+}
