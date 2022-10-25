be7fd7667844dbb6afae2db48f4baa36452961bd
Tomas <tomas.kozakas@gmail.com>
2022-10-25 18:47:24 +0300

    Make better graph, support floating point axes

--- src/main/java/org/calculator/draw/Graph.java
+++ src/main/java/org/calculator/draw/Graph.java
@@ -9,20 +9,16 @@ import java.util.List;
  * @author Tomas Kozakas
  */
 public class Graph extends JPanel {
-    private int maxScore;
-    private static final int PREF_W = 800;
-    private static final int PREF_H = 650;
-    private static final int BORDER_GAP = 30;
-    private static final Color GRAPH_COLOR = Color.green;
-    private static final Color GRAPH_POINT_COLOR = new Color(150, 50, 50, 180);
-    private static final Stroke GRAPH_STROKE = new BasicStroke(3f);
-    private static final int GRAPH_POINT_WIDTH = 12;
-    private static final int Y_HATCH_CNT = 10;
-    private final List<Integer> scores;
+    private final Color lineColor = new Color(44, 102, 230, 180);
+    private final Color pointColor = new Color(100, 100, 100, 180);
+    private final Color gridColor = new Color(200, 200, 200, 200);
+    private static final Stroke GRAPH_STROKE = new BasicStroke(2f);
+    private final List<Double> scores;
 
-    public Graph(List<Integer> scores) {
+    public Graph(List<Double> scores) {
+        scores.add(0.0);
         this.scores = scores;
-        this.maxScore = scores.get(0) * 2;
+        this.setLayout(new BorderLayout());
     }
 
     @Override
@@ -31,40 +27,67 @@ public class Graph extends JPanel {
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        double xScale = ((double) getWidth() - 2 * BORDER_GAP) / (scores.size() - 1);
-        double yScale = ((double) getHeight() - 2 * BORDER_GAP) / (maxScore - 1);
+        int labelPadding = 25;
+        int padding = 25;
+        double xScale = ((double) getWidth() - (2 * padding) - labelPadding) / (scores.size() - 1);
+        double yScale = ((double) getHeight() - 2 * padding - labelPadding) / (getMaxScore() - getMinScore());
 
         List<Point> graphPoints = new ArrayList<>();
         for (int i = 0; i < scores.size(); i++) {
-            int x1 = (int) (i * xScale + BORDER_GAP);
-            int y1 = (int) ((maxScore - scores.get(i)) * yScale + BORDER_GAP);
+            int x1 = (int) (i * xScale + padding + labelPadding);
+            int y1 = (int) ((getMaxScore() - scores.get(i)) * yScale + padding);
             graphPoints.add(new Point(x1, y1));
         }
 
-        // create x and y axes
-        g2.drawLine(BORDER_GAP, getHeight() - BORDER_GAP, BORDER_GAP, BORDER_GAP);
-        g2.drawLine(BORDER_GAP, getHeight() - BORDER_GAP, getWidth() - BORDER_GAP, getHeight() - BORDER_GAP);
+        // draw white background
+        g2.setColor(Color.WHITE);
+        g2.fillRect(padding + labelPadding, padding, getWidth() - (2 * padding) - labelPadding, getHeight() - 2 * padding - labelPadding);
+        g2.setColor(Color.BLACK);
 
-        // create hatch marks for y-axis.
-        for (int i = 0; i < Y_HATCH_CNT; i++) {
-            int x0 = BORDER_GAP;
-            int x1 = GRAPH_POINT_WIDTH + BORDER_GAP;
-            int y0 = getHeight() - (((i + 1) * (getHeight() - BORDER_GAP * 2)) / Y_HATCH_CNT + BORDER_GAP);
+        // create hatch marks and grid lines for y-axis.
+        int pointWidth = 7;
+        int numberYDivisions = 10;
+        for (int i = 0; i < numberYDivisions + 1; i++) {
+            int x0 = padding + labelPadding;
+            int x1 = pointWidth + padding + labelPadding;
+            int y0 = getHeight() - ((i * (getHeight() - padding * 2 - labelPadding)) / numberYDivisions + padding + labelPadding);
+            if (!scores.isEmpty()) {
+                g2.setColor(gridColor);
+                g2.drawLine(padding + labelPadding + 1 + pointWidth, y0, getWidth() - padding, y0);
+                g2.setColor(Color.BLACK);
+                String yLabel = ((int) ((getMinScore() + (getMaxScore() - getMinScore()) * ((i * 1.0) / numberYDivisions)) * 100)) / 100.0 + "";
+                FontMetrics metrics = g2.getFontMetrics();
+                int labelWidth = metrics.stringWidth(yLabel);
+                g2.drawString(yLabel, x0 - labelWidth - 5, y0 + (metrics.getHeight() / 2) - 3);
+            }
             g2.drawLine(x0, y0, x1, y0);
         }
 
-        // and for x-axis
+        // and for x axis
         for (int i = 0; i < scores.size() - 1; i++) {
-            int x0 = (i + 1) * (getWidth() - BORDER_GAP * 2) / (scores.size() - 1) + BORDER_GAP;
-            int y0 = getHeight() - BORDER_GAP;
-            int y1 = y0 - GRAPH_POINT_WIDTH;
+            int x0 = i * (getWidth() - padding * 2 - labelPadding) / (scores.size() - 1) + padding + labelPadding;
+            int y0 = getHeight() - padding - labelPadding;
+            int y1 = y0 - pointWidth;
+            if ((i % ((int) ((scores.size() / 20.0)) + 1)) == 0) {
+                g2.setColor(gridColor);
+                g2.drawLine(x0, getHeight() - padding - labelPadding - 1 - pointWidth, x0, padding);
+                g2.setColor(Color.BLACK);
+                String xLabel = i + 1 + "";
+                FontMetrics metrics = g2.getFontMetrics();
+                int labelWidth = metrics.stringWidth(xLabel);
+                g2.drawString(xLabel, x0 - labelWidth / 2, y0 + metrics.getHeight() + 3);
+            }
             g2.drawLine(x0, y0, x0, y1);
         }
 
+        // create x and y axes
+        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, padding + labelPadding, padding);
+        g2.drawLine(padding + labelPadding, getHeight() - padding - labelPadding, getWidth() - padding, getHeight() - padding - labelPadding);
+
         Stroke oldStroke = g2.getStroke();
-        g2.setColor(GRAPH_COLOR);
+        g2.setColor(lineColor);
         g2.setStroke(GRAPH_STROKE);
-        for (int i = 0; i < graphPoints.size() - 1; i++) {
+        for (int i = 0; i < graphPoints.size() - 2; i++) {
             int x1 = graphPoints.get(i).x;
             int y1 = graphPoints.get(i).y;
             int x2 = graphPoints.get(i + 1).x;
@@ -73,17 +96,34 @@ public class Graph extends JPanel {
         }
 
         g2.setStroke(oldStroke);
-        g2.setColor(GRAPH_POINT_COLOR);
-        for (Point graphPoint : graphPoints) {
-            int x = graphPoint.x - GRAPH_POINT_WIDTH / 2;
-            int y = graphPoint.y - GRAPH_POINT_WIDTH / 2;
-            g2.fillOval(x, y, GRAPH_POINT_WIDTH, GRAPH_POINT_WIDTH);
+        g2.setColor(pointColor);
+        for (int i = 0; i < graphPoints.size() - 1; i++) {
+            int x = graphPoints.get(i).x - pointWidth / 2;
+            int y = graphPoints.get(i).y - pointWidth / 2;
+            g2.fillOval(x, y, pointWidth, pointWidth);
         }
     }
 
+    private Double getMaxScore() {
+        double maxScore = Integer.MIN_VALUE;
+        for (Double score : scores) {
+            maxScore = Math.max(maxScore, score);
+        }
+        return maxScore * 2;
+    }
+
+    private Double getMinScore() {
+        double minScore = Double.MAX_VALUE;
+        for (Double score : scores) {
+            minScore = Math.min(minScore, score);
+        }
+        return minScore;
+    }
+
+
     @Override
     public Dimension getPreferredSize() {
-        return new Dimension(PREF_W, PREF_H);
+        return new Dimension(1200, 800);
     }
 
 }
