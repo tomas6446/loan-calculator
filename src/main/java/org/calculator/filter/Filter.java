76823af1b25bb6082118e9756fe595100bca23f4
Tomas <tomas.kozakas@gmail.com>
2022-10-25 18:47:50 +0300

    Make Filter.java and Table.java return floating point

--- src/main/java/org/calculator/filter/Filter.java
+++ src/main/java/org/calculator/filter/Filter.java
@@ -1,7 +1,7 @@
 package org.calculator.filter;
 
 import org.calculator.loan.Loan;
-import org.calculator.loan.Table;
+import org.calculator.loan.table.Row;
 
 /**
  * @author Tomas Kozakas
@@ -16,14 +16,15 @@ public class Filter implements FilterInterface {
     @Override
     public void getMonthStat() {
         System.out.format("%12s%20s%20s%20s%20s%20s%n", "month", "initial", "payment", "interest", "debt part", "balance left");
-        for (Table table : loan.getFullTable()) {
-            System.out.format("%12d%20.2f%20.2f%20.2f%20.2f%20.2f%n", table.month(), table.initialBalance(), table.payment(), table.interestRate(), table.debtPart(), table.balanceLeft());
+        for (int i = 0; i < loan.getTable().getRows().size(); i++) {
+            Row row = loan.getTable().getRow(i);
+            System.out.format("%12d%20.2f%20.2f%20.2f%20.2f%20.2f%n", i + 1, row.initialBalance(), row.payment(), row.interestRate(), row.debtPart(), row.balanceLeft());
         }
     }
 
-    public void getMonthStat(int index) {
+    public void getMonthStat(int month) {
         System.out.format("%12s%20s%20s%20s%20s%20s%n", "month", "initial", "payment", "interest", "debt part", "balance left");
-        Table table = loan.getFullTable().get(index);
-        System.out.format("%12d%20.2f%20.2f%20.2f%20.2f%20.2f%n", table.month(), table.initialBalance(), table.payment(), table.interestRate(), table.debtPart(), table.balanceLeft());
+        Row row = loan.getTable().getRow(month - 1);
+        System.out.format("%12d%20.2f%20.2f%20.2f%20.2f%20.2f%n", month, row.initialBalance(), row.payment(), row.interestRate(), row.debtPart(), row.balanceLeft());
     }
 }
