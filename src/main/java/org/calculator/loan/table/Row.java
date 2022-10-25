4afa62bd3d8de94a0690a7e043f6da6da201c096
Tomas <tomas.kozakas@gmail.com>
2022-10-25 18:48:04 +0300

    Make Filter.java and Table.java return floating point

---
+++ src/main/java/org/calculator/loan/table/Row.java
@@ -0,0 +1,7 @@
+package org.calculator.loan.table;
+
+/**
+ * @author Tomas Kozakas
+ */
+
+public record Row(double initialBalance, double payment, double interestRate, double debtPart, double balanceLeft) { }
